942c1bad9d91570aee75f0aaa6dd183030a53fb8
yirvine <yene.irvine@ucalgary.ca>
2025-04-02 16:35:12 -0400

    fixed life.jpg page

--- src/app/life/page.tsx
+++ src/app/life/page.tsx
@@ -2,11 +2,12 @@
 
 import ImageGallery from "./components/image-gallery"
 
-export default function LifePage() {
+export default function Life() {
   return (
-    <div className="min-h-screen bg-black text-white p-6">
-      <div className="max-w-7xl mx-auto">
-        <h1 className="text-4xl font-mono mb-8">Life</h1>
+    <div className="min-h-screen bg-black text-white">
+      <div className="max-w-7xl mx-auto px-4 py-8">
+        <h1 className="text-4xl font-mono mb-8">life.jpg</h1>
+        <p className="text-gray-400 mb-8">as seen on my camera roll</p>
         <ImageGallery />
       </div>
     </div>
