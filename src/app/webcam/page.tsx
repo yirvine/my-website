746210b6789760fb552a9c22063e8930d09c89a8
yirvine <yene.irvine@ucalgary.ca>
2025-04-05 13:30:35 -0400

    gallery images

--- src/app/webcam/page.tsx
+++ src/app/webcam/page.tsx
@@ -1,7 +1,7 @@
 export default function Webcam() {
   return (
     <div className="min-h-screen bg-black text-white p-6">
-      <h1 className="text-2xl font-mono">Just kidding.</h1>
+      <h1 className="text-2xl font-mono">just kidding.</h1>
     </div>
   )
 } 
